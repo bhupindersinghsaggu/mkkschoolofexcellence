55f2e30529e0230e835bde30c7bb6c7b44cddba9
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-07-02 10:14:58 +0530

    Update gps-app.php

--- gps-app.php
+++ gps-app.php
@@ -40,12 +40,12 @@ ini_set('display_errors', 1);
                     <h2 class="sec-title position-relative pd-50">Student App For <span class="color">Transport</span></h2>
                     <p class="line-heigth">Please install the Student App to view all the information related to your ward. Use the following credentials to log in:</p>
                     <p class="line-heigth">
-                    <h5>User: <span class="pur-color">Student's Admissions No. </span></h5>
-                    <p> Example: User: <span class="pur-color">2025001 </span></p>
+                    <h5>User: <span class="pur-color"> Student's Admissions No. </span></h5>
+                    <p> <b> For Example: User: </b><span class="pur-color"> 2025001 </span></p>
                     </p>
                     <p class="line-heigth">
-                    <h5>Password:<span class="pur-color">Student's Admissions No.</span></h5>
-                    <p> Example: Pasword: <span class="pur-color">2025001  </span></p>
+                    <h5> Password:<span class="pur-color"> Student's Admissions No.</span></h5>
+                    <p> <b>For Example: Pasword: </b><span class="pur-color"> 2025001 </span></p>
                     </p>
                     <div class="d-sm-flex align-items-center mt-5">
                         <a href="/SOE.apk" class="btn btn-primary shadow">Download APP</a>
