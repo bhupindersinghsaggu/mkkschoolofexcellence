80d105d889e138fcbd0af99a3000601c0bb33261
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-07-18 12:23:54 +0530

    Update header.php

--- web/header.php
+++ web/header.php
@@ -2,6 +2,9 @@
 <html lang="zxx">
 
 <head>
+     <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php echo isset($meta_title) ? $meta_title : "Default Title"; ?></title>
