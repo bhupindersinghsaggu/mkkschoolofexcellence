6b16383b0e9c966e77181c74b9d492c1069b9b9c
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-11-23 18:15:44 +0530

    Update principal-message.php

--- principal-message.php
+++ principal-message.php
@@ -12,7 +12,7 @@ ini_set('display_errors', 1);
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1 class="display-2 text-white">Chairman Message</h1>
+                <h1 class="display-2 text-white">Principal Message</h1>
             </div>
         </div>
     </div>
@@ -21,7 +21,17 @@ ini_set('display_errors', 1);
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                
+                <p class="line-heigth">Greetings and welcome to the School of Excellence !
+
+At the School of Excellence, we are more than just a place of learning—we are a student centered community dedicated to nurturing hearts, shaping minds, and preparing young lives to shine as lights in the world.
+
+Rooted in values, our vision is to raise up confident, compassionate, and capable students who live with purpose and integrity. We believe that true excellence begins with a heart aligned to God and is reflected in every area of life—academics, relationships, service, and personal growth.
+
+In today’s changing world, we embrace a Hybrid Model of Education that combines the richness of classroom experience with the flexibility and innovation of digital learning. This approach allows us to reach learners wherever they are, while still cultivating a strong sense of community and connection.
+
+Our dedicated educators are not only academically qualified, but also ethically grounded, committed to walking alongside each child in their unique journey of learning.
+
+As you explore our school, we pray you’ll see a place where your child can grow in wisdom and stature. We invite you to partner with us in this task of education—where learning leads to transformation.</p>
             </div>
             <div class="col-lg-4">
                 <aside class="sidebar ps-lg-4">
