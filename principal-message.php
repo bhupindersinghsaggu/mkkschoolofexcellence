e19e300e26c85ef9818e7e8aff36d073be49a394
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-11-23 18:31:32 +0530

    Update principal-message.php

--- principal-message.php
+++ principal-message.php
@@ -21,12 +21,12 @@ ini_set('display_errors', 1);
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
-               <div class="choose-media position-relative">
+                <div class="choose-media position-relative">
                     <img src="images/principal.jpg" alt="About" class="img-fluid">
                 </div>
             </div>
             <div class="col-lg-8">
-                <p class="line-heigth">Greetings and welcome to the School of Excellence !</p>
+                <h3 class="display-5">Greetings and welcome to the School of Excellence !</h3>
                 <p class="line-heigth"> At the School of Excellence, we are more than just a place of learning—we are a student centered community dedicated to nurturing hearts, shaping minds, and preparing young lives to shine as lights in the world.
                     Rooted in values, our vision is to raise up confident, compassionate, and capable students who live with purpose and integrity. We believe that true excellence begins with a heart aligned to God and is reflected in every area of life—academics, relationships, service, and personal growth.
                     In today’s changing world, we embrace a Hybrid Model of Education that combines the richness of classroom experience with the flexibility and innovation of digital learning. This approach allows us to reach learners wherever they are, while still cultivating a strong sense of community and connection.
