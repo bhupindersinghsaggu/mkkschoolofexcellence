6b3aa45e2f2385c5717b2e878bd36623aa79d0e3
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-11-23 18:19:40 +0530

    about us link added

--- principal-message.php
+++ principal-message.php
@@ -29,35 +29,9 @@ ini_set('display_errors', 1);
                     As you explore our school, we pray you’ll see a place where your child can grow in wisdom and stature. We invite you to partner with us in this task of education—where learning leads to transformation.</p>
             </div>
             <div class="col-lg-4">
-                <aside class="sidebar ps-lg-4">
-                    <div class="widget search-widget">
-                        <form action="#" class="position-relative">
-                            <input type="search" placeholder="Search keyword">
-                            <button class="btn btn-primary position-absolute  top-0"><i class="feather-icon icon-search"></i></button>
-                        </form>
-                    </div>
-                    <div class="widget category-widget">
-                        <h3 class="widget-title">Popular Categories</h3>
-                        <div class="widget-inner">
-                            <ul>
-                                <li><a href="history.php">History<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                                <li><a href="vision-mission.php">Vision & Mission<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                                <li><a href="principal-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                                <li><a href="chairman-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="widget widget_author">
-                        <div class="widget-inner p-4 text-center">
-                            <h4 class="display-5">Follow us</h4>
-                            <div class="footer-social mt-4">
-                                <a href="#"><i class="feather-icon icon-facebook"></i></a>
-                                <a href="#"><i class="feather-icon icon-instagram"></i></a>
-                                <a href="#"><i class="feather-icon icon-youtube"></i></a>
-                            </div>
-                        </div>
-                    </div><!-- Widget End -->
-                </aside>
+                <?php
+                include ('/web/about-links.php')
+                ?>
             </div>
         </div>
     </div>
---
+++ web/about-links.php
@@ -0,0 +1,29 @@
+<aside class="sidebar ps-lg-4">
+    <div class="widget search-widget">
+        <form action="#" class="position-relative">
+            <input type="search" placeholder="Search keyword">
+            <button class="btn btn-primary position-absolute  top-0"><i class="feather-icon icon-search"></i></button>
+        </form>
+    </div>
+    <div class="widget category-widget">
+        <h3 class="widget-title">Popular Categories</h3>
+        <div class="widget-inner">
+            <ul>
+                <li><a href="history.php">History<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+                <li><a href="vision-mission.php">Vision & Mission<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+                <li><a href="principal-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+                <li><a href="chairman-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="widget widget_author">
+        <div class="widget-inner p-4 text-center">
+            <h4 class="display-5">Follow us</h4>
+            <div class="footer-social mt-4">
+                <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                <a href="#"><i class="feather-icon icon-youtube"></i></a>
+            </div>
+        </div>
+    </div><!-- Widget End -->
+</aside>
