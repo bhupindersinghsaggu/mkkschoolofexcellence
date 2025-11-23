072b5fde8c176e857c7a92d2607b8da4f38fa8ad
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-11-23 18:22:03 +0530

    Update about-links.php

--- web/about-links.php
+++ web/about-links.php
@@ -6,13 +6,13 @@
         </form>
     </div>
     <div class="widget category-widget">
-        <h3 class="widget-title">Popular Categories</h3>
+        <h3 class="widget-title">More Links</h3>
         <div class="widget-inner">
             <ul>
                 <li><a href="history.php">History<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
                 <li><a href="vision-mission.php">Vision & Mission<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
                 <li><a href="principal-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                <li><a href="chairman-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+                <!-- <li><a href="chairman-message.php">Message From Chairman<span><i class="feather-icon icon-chevron-right"></i></span></a></li> -->
             </ul>
         </div>
     </div>
@@ -20,9 +20,9 @@
         <div class="widget-inner p-4 text-center">
             <h4 class="display-5">Follow us</h4>
             <div class="footer-social mt-4">
-                <a href="#"><i class="feather-icon icon-facebook"></i></a>
-                <a href="#"><i class="feather-icon icon-instagram"></i></a>
-                <a href="#"><i class="feather-icon icon-youtube"></i></a>
+                <a href="https://www.facebook.com/schoolofexcellencepanipat"><i class="feather-icon icon-facebook"></i></a>
+                <a href="https://www.instagram.com/schoolofexcellencepanipat2025/"><i class="feather-icon icon-instagram"></i></a>
+                <!-- <a href="#"><i class="feather-icon icon-youtube"></i></a> -->
             </div>
         </div>
     </div><!-- Widget End -->
