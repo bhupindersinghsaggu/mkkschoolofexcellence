dee15ab8caf4319675884821b74f71dfa4a2fada
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-11-23 17:06:09 +0530

    Update header.php

--- web/header.php
+++ web/header.php
@@ -15,11 +15,8 @@
     <link rel="stylesheet" href="css/nice-select2.css">
     <link href="css/glightbox.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/swiper-bundle.min.css">
-    <!-- Style css -->
     <link rel="stylesheet" href="css/style.css">
-    <!-- Custom css -->
     <link rel="stylesheet" href="css/custom.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
