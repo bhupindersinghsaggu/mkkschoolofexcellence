8699cc9a09505668d25ed943c2e87f4286b5cbfd
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-02 14:41:26 +0530

    Update indoor-outdoor-games.php

--- indoor-outdoor-games.php
+++ indoor-outdoor-games.php
@@ -26,14 +26,24 @@ ini_set('display_errors', 1);
                 </div>
                 <p class="line-heigth pt-4">At School of Excellence, we believe that education is incomplete without physical fitness and recreational activities. As the saying goes, “All work and no play makes Jack a dull boy,” which is why our curriculum gives equal importance to sports, games, and overall well-being. Students participate in a wide range of outdoor sports such as soccer, basketball, volleyball, and more — helping them develop stamina, teamwork, discipline, and a healthy spirit of competition.
                 </p>
-                <p class="line-heigth">Along with outdoor activities, we also emphasize indoor games that stimulate mental alertness and analytical thinking. Games like table tennis, carrom, chess, puzzles, scrabble, ludo, monopoly, and others help enhance focus, problem-solving skills, and strategic thinking.
-                </p>
-                <p class="line-heigth">Through both indoor and outdoor sports, School of Excellence ensures that students get the perfect balance of physical fitness, mental strength, enjoyment, and stress relief. We aim to nurture learners who are not only academically strong but also emotionally balanced, energetic, and confident in every aspect of life.</p>
+                <div class="row">
+                    <div class="col-lg-6">
+ss
+                    </div>
+                    <div class="col-lg-6">
+ss
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <?php include('web/infra-links.php'); ?>
-            </div>
+
+            <p class="line-heigth">Along with outdoor activities, we also emphasize indoor games that stimulate mental alertness and analytical thinking. Games like table tennis, carrom, chess, puzzles, scrabble, ludo, monopoly, and others help enhance focus, problem-solving skills, and strategic thinking.
+            </p>
+            <p class="line-heigth">Through both indoor and outdoor sports, School of Excellence ensures that students get the perfect balance of physical fitness, mental strength, enjoyment, and stress relief. We aim to nurture learners who are not only academically strong but also emotionally balanced, energetic, and confident in every aspect of life.</p>
         </div>
+        <div class="col-lg-4">
+            <?php include('web/infra-links.php'); ?>
+        </div>
+    </div>
     </div>
 </section>
 <?php include('web/footer.php'); ?>
