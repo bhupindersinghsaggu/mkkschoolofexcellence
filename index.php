d7cc6d3f5edce50521521b87f75b8a6de8f36776
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-10 14:48:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -560,9 +560,7 @@ error_reporting(E_ALL);
                <h3 class="display-5">Greetings and welcome to the School of Excellence !</h3>
                <p class="line-heigth"> At the School of Excellence, we are more than just a place of learning—we are a student centered community dedicated to nurturing hearts, shaping minds, and preparing young lives to shine as lights in the world.
                   Rooted in values, our vision is to raise up confident, compassionate, and capable students who live with purpose and integrity. We believe that true excellence begins with a heart aligned to God and is reflected in every area of life—academics, relationships, service, and personal growth.
-                  In today’s changing world, we embrace a Hybrid Model of Education that combines the richness of classroom experience with the flexibility and innovation of digital learning. This approach allows us to reach learners wherever they are, while still cultivating a strong sense of community and connection.
-                  Our dedicated educators are not only academically qualified, but also ethically grounded, committed to walking alongside each child in their unique journey of learning.
-                  As you explore our school, we pray you’ll see a place where your child can grow in wisdom and stature. We invite you to partner with us in this task of education—where learning leads to transformation.</p>
+                  In today’s changing world, we embrace a Hybrid Model of Education that combines the richness of classroom experience with the flexibility and innovation of digital learning...<a href="principal-message.php"> Read more </a></p>
                <h3 class="display-5"> Dr. Hepesh Shepherd</h3>
                <p> Principal</p>
             </div>
