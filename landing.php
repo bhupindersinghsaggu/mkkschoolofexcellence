90ddab0412dab6a2b1c01278da494f10dd643259
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-15 21:23:17 +0530

    Update landing.php

--- landing.php
+++ landing.php
@@ -486,10 +486,7 @@ error_reporting(E_ALL);
                <div class="contact-txt">
                   <span class="badge-lg rounded-5 bg-primary">Get in Touch</span>
                   <h2 class="sec-title text-info">Talk to  <span class="color">Our Couseller</span></h2>
-                  <!-- <p>
-                     Being involved in education dedicatedly is a good meditation for years. A good education for you and me,
-                     a better world it will be.
-                  </p> -->
+                 <img class="ab-media-1 img-fluid " src="images/about-n1.png" alt="About">
                </div>
             </div>
          </div>
