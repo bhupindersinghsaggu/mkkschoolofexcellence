c525cef9a9a3d9e59ecb0c048cbabd034627cc0e
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-15 16:09:51 +0530

    add animationa

--- landing.php
+++ landing.php
@@ -38,7 +38,7 @@ error_reporting(E_ALL);
                         <h2 class="sec-title position-relative pd-50">
                            Admission Open <span class="color">2026-27</span>
                         </h2>
-                        <span class="badge-lg bg-primary rounded-5">No Admission Fee</span>
+                        <span class="badge-lg bg-primary rounded-5 bounce">No Admission Fee</span>
                         <iframe style="height:600px;" width="100%"
                            src="https://curtina.in/_EazySchool/FormsWeb/frmEnquiry_Mkk.aspx?AppId=SOE">
                         </iframe>
