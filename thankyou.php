61b119c32aa9fb98fd501f0befdc35014d1841fa
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-15 15:11:37 +0530

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -13,22 +13,25 @@ $meta_keywords = "Contact MKK School Panipat, School Address, CBSE School Near M
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
-<section class="promo-sec" style="background: url('images/promo-bg.jpg')no-repeat center center / cover;">
+<!-- <section class="promo-sec" style="background: url('images/promo-bg.jpg')no-repeat center center / cover;">
     <img src="images/promo-left.png" alt="" class="anim-img">
     <img src="images/promo-right.png" alt="" class="anim-img anim-right">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1 class="display-2 text-white">Contact</h1>
+                <h1 class="display-2 text-white">Than</h1>
             </div>
         </div>
     </div>
-</section>
+</section> -->
 <section class="single-post sec-padding">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                
+
+            <h2 class="sec-title mb-4 labs-heading">
+                “Thank you for choosing School of Excellence! Our team will contact you shortly. For any further queries, you may call us at  
+                <a href="tel:+91 90342 52500">+91 90342 52500</a></h2>
             </div>
         </div>
     </div>
