f8017546fed4ec8058d4022418aa6025a9a9e169
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-16 13:39:06 +0530

    Update landing.php

--- landing.php
+++ landing.php
@@ -12,7 +12,9 @@ error_reporting(E_ALL);
       <div class="container">
          <div class="row">
             <div class="d-flex justify-content-center">
+               <div>
                <a class="navbar-brand" href="landing.php"><img src="images/logo.png" alt="Logo"></a>
+               </div>
                <h1 class="banner-sec2 h1 hero-heading">
                   School of <span class="color">Excellence</span>
                </h1>
