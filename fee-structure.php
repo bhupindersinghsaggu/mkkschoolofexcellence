609b8a097d55c295acbed4d0e797d7559ca97ce8
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-17 12:44:48 +0530

    Update fee-structure.php

--- fee-structure.php
+++ fee-structure.php
@@ -23,14 +23,14 @@ ini_set('display_errors', 1);
             <div class="col-sm-6">
                 <h5> Registration fee ₹1000</h5>
                 <!-- <p class="line-heigth"> Admission fee ₹30000 ( waived for initial 100 admissions for 2026 -27 )</p> -->
-                <p class="line-heigth"> Tuition fee ( PreN to Prep ) - ₹6000/-</p>
-                <p class="line-heigth"> Classes 1 to 5 - ₹7250/-</p>
-                <p class="line-heigth"> Class 6 - ₹7500/-</p>
-                <p class="line-heigth"> Class 7 - ₹7750/-</p>
-                <p class="line-heigth">A Sibling discount of 20% on the Tuition fee of the elder sibling.</p>
+                <p class="line-heigth"><i class="fa-solid fa-circle-arrow-right"></i> Tuition fee ( PreN to Prep ) - ₹6000/-</p>
+                <p class="line-heigth"> <i class="fa-solid fa-circle-arrow-right"></i> Classes 1 to 5 - ₹7250/-</p>
+                <p class="line-heigth"> <i class="fa-solid fa-circle-arrow-right"></i> Class 6 - ₹7500/-</p>
+                <p class="line-heigth"> <i class="fa-solid fa-circle-arrow-right"></i> Class 7 - ₹7750/-</p>
+                <p class="line-heigth"><i class="fa-solid fa-circle-arrow-right"></i> A Sibling discount of 20% on the Tuition fee of the elder sibling.</p>
                 <h5> Transport charges</h5>
-                <p class="line-heigth"> 1800/- within 1 KM</p>
-                <p class="line-heigth"> 2200/- afterwards</p>
+                <p class="line-heigth"><i class="fa-solid fa-circle-arrow-right"></i>  1800/- within 1 KM</p>
+                <p class="line-heigth"> <i class="fa-solid fa-circle-arrow-right"></i> 2200/- afterwards</p>
             </div>
         </div>
     </div>
