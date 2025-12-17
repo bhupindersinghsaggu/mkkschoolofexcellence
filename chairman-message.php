5907016109fca0468c14c86231bbc5e238c81eaf
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-17 12:32:36 +0530

    Update chairman-message.php

--- chairman-message.php
+++ chairman-message.php
@@ -37,8 +37,7 @@ ini_set('display_errors', 1);
                         <h3 class="widget-title">More Links</h3>
                         <div class="widget-inner">
                             <ul>
-                                <li><a href="history.php">History<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
-                                <li><a href="vision-mission.php">Vision & Mission<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
+                                <li><a href="about-us.php">About us <span><i class="feather-icon icon-chevron-right"></i></span></a></li>
                                 <li><a href="principal-message.php">Message From Principal<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
                                 <li><a href="chairman-message.php">Message From Chairman<span><i class="feather-icon icon-chevron-right"></i></span></a></li>
                             </ul>
