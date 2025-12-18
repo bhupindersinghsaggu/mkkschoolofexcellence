ddd693a1b94b78a6428190a6da8edbc1680eafd3
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-12-18 15:41:20 +0530

    Update landing.php

--- landing.php
+++ landing.php
@@ -36,7 +36,8 @@ error_reporting(E_ALL);
          <div class="row ">
             <div class="d-flex justify-content-center gap-5  mobile-row">
                <div class="about-media-2 position-relative mb-4 mb-xl-0">
-                  <img class="img-fluid" src="images/hero1.png" alt="About">
+                  <img class="img-fluid" src="images/hero1.png" alt="About" style="    border: 2px solid #6045ff;
+    border-radius: 23px;">
                </div>
                <div>
                   <h2 class="sec-title position-relative">
